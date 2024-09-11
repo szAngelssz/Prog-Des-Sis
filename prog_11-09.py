2bea3eb82639bb8f7e4f5b52239e3d24c4c9bbb1
Angel <asrieldreemurr.ut2019@gmail.com>
2024-09-11 11:03:29 -0300

    Adding the file "prog_11-09.py"

---
+++ prog_11-09.py
@@ -0,0 +1,11 @@
+# Aprendendo a fazer um loop
+
+# Iniciando a variável
+
+contador = 0
+
+# Utilizando while
+
+while contador < 50:
+    print(f'O valor do contador é: {contador}')
+    contador += 1
